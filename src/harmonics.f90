b055ff8f6bd4f26accbcdfd38fad83192593a3d0
Eric Heien <emheien@ucdavis.edu>
2013-02-02 19:47:02 -0800

    Changed failure to use exit code rather than file

--- src/harmonics.f90
+++ src/harmonics.f90
@@ -132,13 +132,9 @@
 ! --- Creates a STOP file that stops the execution of makeseles.sh 
 !     ******************* GS and FC 13-12-2007 *******************
 !
-	open (11,file='stop.dat',status='new')
-	Write(11,*) "Errors have been detected - SELEN will stop"
-	close(11)
-!
 	write(* ,*) 'STOP_CONFIG: SELEN will STOP!' 
 	write(88,*) 'STOP_CONFIG: SELEN will STOP!' 
-	STOP 
+	STOP 1
 !
 	End subroutine STOP_CONFIG 	
 !
